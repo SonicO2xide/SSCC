0f3d96a42df0b282585a5ba55683fc1035773b42
SonicO2xide <68080425+SonicO2xide@users.noreply.github.com>
2020-08-16 14:31:04 -0700

    Delete startRelayNode1.sh

--- startRelayNode1.sh
+++
@@ -1,8 +0,0 @@
-DIRECTORY=$NODE_HOME/relaynode1
-PORT=3001
-HOSTADDR=68.183.203.145
-TOPOLOGY=${DIRECTORY}/mainnet-topology.json
-DB_PATH=${DIRECTORY}/db
-SOCKET_PATH=${DIRECTORY}/db/socket
-CONFIG=${DIRECTORY}/mainnet-config.json
-cardano-node run --topology ${TOPOLOGY} --database-path ${DB_PATH} --socket-path ${SOCKET_PATH} --host-addr ${HOSTADDR} --port ${PORT} --config ${CONFIG}
